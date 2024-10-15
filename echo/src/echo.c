7c918558b9b61ce6e533fd4ef8da9e117f05be20
E.M. From <vertlaforet@protonmail.com>
2024-10-15 17:45:17 +0200

    Changed argument parsing logic

--- echo/src/echo.c
+++ echo/src/echo.c
@@ -6,7 +6,7 @@
  * Original by Vasco Costa
  * Modifications by E.M. From
  *
- * Very straight forward. Options recognized are -n to not add a newline to output
+ * Options recognized are -n to not add a newline to output
  * and -h for help
  *
  */
@@ -14,6 +14,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * Helper funcion to print a help message
@@ -33,33 +35,51 @@ void show_usage (char *prog_name)
  */
 int main (int argc, char *argv[])
 {
+    //Add newline to end of printout
     bool newline = true;
 
-  /** Loop over all arguments */
-    if (argc > 1)
+    //Start on which argument
+    int first_arg_toprint = 1;
+
+  /** Check if we have arguments */
+    if (argc == 1)
     {
-        for (int i = 1; i < argc; i++)
+        //Nope, print help message
+        show_usage (argv[0]);
+        return EXIT_SUCCESS;
+    }
+
+  /** Check for "-" in first argument */
+    if (*argv[1] == '-')
+    {
+        //Check if it's -n
+        if (tolower (*(argv[1] + 1)) == 'n')
+        {                       //*(argv[1] + ) means 2nd letter in argv[1]
+            newline = false;
+        }
+        else
         {
-            if (strcmp (argv[i], "-h") == 0)
-            {
-                show_usage (argv[0]);
+            //Any other letter after - means show help
+            // If it's h, user wanted to see help
+            // If it's something else, we dont support that
+            show_usage (argv[0]);
+            return EXIT_SUCCESS;
+        }
 
-                return 0;
-            }
+        //Start printing from 2nd argument
+        first_arg_toprint = 2;
+    }
 
-            if (strcmp (argv[i], "-n") == 0)
-            {
-                newline = false;
 
-                continue;
-            }
+  /** Print the remaining arguments */
+    for (int i = first_arg_toprint; i < argc; i++)
+    {
+        printf ("%s", argv[i]);
 
-            printf ("%s", argv[i]);
-
-            if (i < argc - 1)
-            {
-                printf (" ");
-            }
+        //Add spaces, unless we are printing last argument
+        if (i < argc - 1)
+        {
+            printf (" ");
         }
     }
 
