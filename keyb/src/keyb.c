fbb54bd3023c090562fb71b2f91f14da6eb57106
E.M. From <vertlaforet@protonmail.com>
2024-10-16 21:36:28 +0200

    More comments + small touchups

--- keyb/src/keyb.c
+++ keyb/src/keyb.c
@@ -16,15 +16,15 @@ void show_usage (char *prog_name)
 {
     printf ("Usage: %s layout\n\n", prog_name);
     printf ("Where layout is one of:\n");
-    printf ("0     UK     UK\n");
-    printf ("1     US     US\n");
+    printf ("0     UK     United Kingdom\n");
+    printf ("1     US     USA\n");
     printf ("2     DE     German\n");
     printf ("3     IT     Italian\n");
     printf ("4     ES     Spanish\n");
     printf ("5     FR     French\n");
     printf ("6     BE     Belgian\n");
     printf ("7     NO     Norwegian\n");
-    printf ("8     JP     Japanese \n");
+    printf ("8     JP     Japanese\n");
     printf ("9     USI    US International\n");
     printf ("10    USA    US International Alternate\n");
     printf ("11    CHD    Swiss German\n");
@@ -66,16 +66,13 @@ const int number_of_layouts = 18;
 
 /**
  * main() using arguments
- * Extended argument processing (AgDev) is used
+ * Extended argument processing (AgDev) is not used
  * 
  */
 int main (int argc, char *argv[])
 {
-    printf ("%d\n", number_of_layouts);
-    exit (0);
 
-
-    //Check if we have and argument
+    //Check if we have an argument
     if (argc != 2)
     {
         show_usage (argv[0]);
@@ -106,13 +103,19 @@ int main (int argc, char *argv[])
         }
     }
 
+  /** Found a locale to set? */
     if (locale == -1)
     {
+        //Nope, exit with help
         show_usage (argv[0]);
         return EXIT_FAILURE;
     }
 
+  /** Set the locale */
     vdp_set_keyboard_locale (locale);
 
+  /** Report change in layout */
     printf ("\nLayout set: %s\n", layout_names[locale]);
+
+    return EXIT_SUCCESS;
 }
